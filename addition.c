d7bc49bb480ce9642306544c847d5a5823e08bf7
Shravhya Priya <shravhyapriya3@gmail.com>
2026-05-16 12:22:26 +0530

    Create addition.c
    .c extension

---
+++ addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{	
+	printf("Name:Shravhya Priya\n");
+	printf("USN:1JS25CI087\n");
+	printf("Program:A robort needs to find how far it must travel bertwwen two points on a 2D plane.Develop a C Program to calculate the straight-line distance between the given coordinates.\n");
+
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	float x1,y1,x2,y2,distancve;
+	printf("enter x1 and y1(coordintes of the first point):");
+	scanf("%f%f",&x1,&y1);
+	printf("enter x2 and y2(coordintes of the first point):");
+	scanf("%f%f",&x2,&y2);
+	distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+	printf("The straight-line distance between the two points is%.2f\n",distance);
